a746481f8203b9e95259efb42c36dfe11719213c
kimTyun <ktyy0511@gmail.com>
2025-05-28 18:20:22 +0900

    asdf

--- nodejs/prac.js
+++ nodejs/prac.js
@@ -1,18 +1,11 @@
 const fs = require('fs')
-const input = fs
-   .readFileSync('./예제.txt')
-   .toString()
-   .trim()
-   .split('\r\n')
-   .map((e) => e.split(' ').splice(1))
-   .filter((e) => e[1] != 'P')
+const input = fs.readFileSync('./예제.txt').toString().trim().split('\r\n')
 
-const grade = { 'A+': 4.5, A0: 4.0, 'B+': 3.5, B0: 3.0, 'C+': 2.5, C0: 2.0, 'D+': 1.5, D0: 1.0, F: 0 }
+const [N, M] = input[0]
 
-let sum1 = 0
-let sum2 = 0
-for (let i of input) {
-   sum1 += Number(i[0]) * grade[i[1]]
-   sum2 += Number(i[0])
-}
-console.log(sum1 / sum2)
+const matrixA = input.slice(1, Number(N) + 1).map((e) => e.split(' ').map(Number))
+const matrixB = input.slice(Number(N) + 1).map((e) => e.split(' ').map(Number))
+
+const solve = matrixA.map((col, i) => col.map((e, j) => (e += matrixB[i][j])).join(' ')).join('\n')
+
+console.log(solve)
