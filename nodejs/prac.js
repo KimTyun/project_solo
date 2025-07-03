ebeff535548063c6eff399e19cc29e5172e225d3
kimTyun <ktyy0511@gmail.com>
2025-07-03 13:04:53 +0900

    a

--- nodejs/prac.js
+++ nodejs/prac.js
@@ -1,31 +1,12 @@
 const fs = require('fs')
 const path = require('path')
 const filePath = path.join(__dirname, '예제.txt')
-const input = fs.readFileSync(filePath).toString().trim().split(' ').map(Number)
+const input = Number(fs.readFileSync(filePath).toString().trim())
 
-const [value, baseN] = input
+console.log(input)
 
-// baseN으로 나눈 몫이 baseN보다 크면 baseN**2로 나눈다. 이게 baseN보다 크면 baseN**3으로 나눈다....
-// 나눠서 몫이 baseN보다 작으면 나머지를 저장한다(첫번째 자리의 값).그후 나머지를 리턴한다
-// 리턴받은 나머지의 몫을 저장하면서 재귀를 나온다.
-let count = 0
-let solve = []
-
-function recursion(value, baseN) {
-   if (value / baseN ** count < baseN) {
-      solve.push(Math.floor(value / baseN ** count)) //첫번째 자리
-      if (count == 0) return
-      return value % baseN ** count--
-   } else {
-      count++
-      let a = recursion(value, baseN)
-      solve.push(Math.floor(a / baseN ** count))
-      if (count == 0) return
-      return a % baseN ** count--
-   }
+// 1,1+6,1+6+6*1,1+6+6*2 ...
+let sol = 1
+for (let i = 1; true; i++) {
+   break
 }
-recursion(value, baseN)
-
-const solve2 = solve.map((e) => (e >= 10 ? String.fromCodePoint(e + 55) : e)).join('')
-
-console.log(solve2)
