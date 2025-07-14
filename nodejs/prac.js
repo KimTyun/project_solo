d518d7186810d2592e3cc9aab4a7ed49da03c664
kimTyun <ktyy0511@gmail.com>
2025-07-14 18:16:58 +0900

    0714

--- nodejs/prac.js
+++ nodejs/prac.js
@@ -1,8 +1,6 @@
 const fs = require('fs')
 const path = require('path')
 const filePath = path.join(__dirname, '예제.txt')
-const input = Number(fs.readFileSync(filePath).toString().trim())
+const input = fs.readFileSync(filePath).toString().split('#')
 
 console.log(input)
-
-1, 2, 4, 7, 11
